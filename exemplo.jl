4fa65c0663c91453d063006840145563ccee70ae
VitorBSP <vitor.pereira@hartbgroup.com.br>
2022-10-11 18:09:50 -0300

    inicio em julia :)

---
+++ exemplo.jl
@@ -0,0 +1,30 @@
+2 + 3
+
+f(x) = x
+
+f(2)
+f(3)
+
+f.(2:0.1:3)
+
+# ] -> pkg mode
+# ctrl + C -> back to julia terminal
+
+using Plots, DataFrames, XLSX
+p = joinpath(@__DIR__, "GC.xlsx")
+xf = XLSX.readxlsx(p)
+xf
+XLSX.sheetnames(xf)
+sh = xf["Planilha1"]
+sh
+
+df = DataFrame(XLSX.readtable(p, "Planilha1"))
+
+m = XLSX.readdata(p, "Planilha1!A1:K29")
+
+df
+m
+m = convert(Matrix{Float64}, m)
+
+m = Matrix(sh)
+transpose(m)
